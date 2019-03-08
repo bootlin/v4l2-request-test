26de1c39fba4ad1b94e4ba1553e41b232b1e9680
Ralf Zerres <ralf.zerres@networkx.de>
2019-03-08 10:24:41 +0100

    print a descriptive response messages stating the codec name
    Signed-off-by: Ralf Zerres <ralf.zerres@networkx.de>

--- v4l2-request-test.c
+++ v4l2-request-test.c
@@ -117,20 +117,16 @@ static void print_summary(struct config *config, struct preset *preset)
 	printf(" Height: %d\n", preset->height);
 	printf(" Frames count: %d\n", preset->frames_count);
 
-	printf(" Format: ");
+	printf(" Codec Type:   ");
 
 	switch (preset->type) {
 	case CODEC_TYPE_MPEG2:
-		printf("MPEG2");
-		break;
 	case CODEC_TYPE_H264:
-		printf("H264");
-		break;
 	case CODEC_TYPE_H265:
-		printf("H265");
+		printf("%s", codec[preset->type].name);
 		break;
 	default:
-		printf("Invalid");
+		printf("Invalid codec type!");
 		break;
 	}
 
